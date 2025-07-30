7b88af6d0149a396d1dabd3aa0bba0e423492728
TeamBearCrew <pandakenfire@gmail.com>
2025-07-30 11:13:41 +0300

    Deleted the userID field because it was necessary.

--- src/main/kotlin/com/mkb3ar/telegram/entity/User.kt
+++ src/main/kotlin/com/mkb3ar/telegram/entity/User.kt
@@ -3,8 +3,7 @@ package com.mkb3ar.telegram.entity
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(value = "user")
-data class User(val id: String,
-                val userID: Long,
+data class User(val id: Long,
                 val firstName: String?,
                 val lastName: String?,
                 val userName: String?)
